f1361f8330d180266b4588a39ca940789c1f7ac5
Andres A Garcia P <codigotutor@gmail.com>
2013-02-08 09:20:07 -0500

    Version inicial. Sin aplicarle ningun capitulo.
    Carpetas excluidas para bajar el tamaño de 80 megas a 7 megas:
    
    TrabajadoresEnojados\TrabajadoresEnojados-html\gwt-unitCache
    TrabajadoresEnojados\TrabajadoresEnojados-html\war\WEB-INF\lib
    TrabajadoresEnojados\TrabajadoresEnojados-ios\libs\ios

---
+++ TrabajadoresEnojados/TrabajadoresEnojados-html/src/us/andresgarcia/trabajadoresenojados/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package us.andresgarcia.trabajadoresenojados.client;
+
+import us.andresgarcia.trabajadoresenojados.TrabajadoresEnojadosGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new TrabajadoresEnojadosGame();
+	}
+}
---
+++ TrabajadoresEnojados/TrabajadoresEnojados/src/us/andresgarcia/trabajadoresenojados/TrabajadoresEnojadosGame.java
@@ -0,0 +1,34 @@
+package us.andresgarcia.trabajadoresenojados;
+
+import com.badlogic.gdx.ApplicationListener;
+
+
+public class TrabajadoresEnojadosGame implements ApplicationListener {
+
+	@Override
+	public void create() {		
+		
+	}
+
+	@Override
+	public void dispose() {
+	
+	}
+
+	@Override
+	public void render() {		
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
